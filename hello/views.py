0f91cd97b6f01f193d76c4e45b9ef9125025fdbf
saikumar <saikumarda4@gmail.com>
2018-07-02 18:29:25 +0530

    d.a
    D,A;SL

--- hello/views.py
+++ hello/views.py
@@ -18,36 +18,36 @@ import time
 def index(request):
     
 	
-	driver = webdriver.Chrome("F:\\z\\chromedriver_win32\\chromedriver.exe")
+		driver = webdriver.Chrome("F:\\z\\chromedriver_win32\\chromedriver.exe")
 
-driver.get('https://www.naukri.com/nlogin/login')
-time.sleep(5)
-print("Opened naukri...")
+	driver.get('https://www.naukri.com/nlogin/login')
+	time.sleep(5)
+	print("Opened naukri...")
 
-text = driver.find_element_by_id('usernameField')
-text.send_keys('saikumarda4@gmail.com')
-print("email entered...")
-password = driver.find_element_by_id('passwordField')
-password.send_keys('sai521sai')
-print("Password entered...")
+	text = driver.find_element_by_id('usernameField')
+	text.send_keys('saikumarda4@gmail.com')
+	print("email entered...")
+	password = driver.find_element_by_id('passwordField')
+	password.send_keys('sai521sai')
+	print("Password entered...")
 
-password.submit()
-print("facebook opened")
+	password.submit()
+	print("facebook opened")
 
-time.sleep(5)
-driver.get('https://www.naukri.com/mnjuser/profile?id=&altresid')
+	time.sleep(5)
+	driver.get('https://www.naukri.com/mnjuser/profile?id=&altresid')
 
-time.sleep(5)
+	time.sleep(5)
 
-link = driver.find_element_by_xpath("//a[contains(.,'Update')]")
-link.click()
-time.sleep(5)
-uploadFile = driver.find_element_by_xpath("//input[@id='attachCV']")
-uploadFile.send_keys('F:\\z\\resume-s.pdf')
-time.sleep(5)
+	link = driver.find_element_by_xpath("//a[contains(.,'Update')]")
+	link.click()
+	time.sleep(5)
+	uploadFile = driver.find_element_by_xpath("//input[@id='attachCV']")
+	uploadFile.send_keys('F:\\z\\resume-s.pdf')
+	time.sleep(5)
 
-print("post done")
-return HttpResponse('Hello from Python!')
+	print("post done")
+	return HttpResponse('Hello from Python!')
     #return render(request, 'index.html')
 
 
